86c08f3f68a7a025541202d5e10fa285fa0ad7c0
Cliff Myrtil <142364326+vocei@users.noreply.github.com>
2023-08-29 17:42:39 -0400

    Add files via upload
    Due to inability to read code, I manually uploaded both exercises to this repository for view.

---
+++ Selection-Statements-Exercise/Selection-Statement-Exercise-2/SelectionStatementsExercise2/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace SelectionStatementsExercise2
+{
+    public class Program
+    {
+        // Switch Case Assignment
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite subject?");
+
+            string subject = Console.ReadLine();
+
+            switch(subject.ToLower()) 
+            {
+                case "math":
+                case "calculus":
+                    Console.WriteLine("I hate Math..(I really do)");
+                    break;
+                case "science":
+                case "physics":
+                case "biology":
+                    Console.WriteLine("Science is kinda cool..");
+                    break;
+                case "PE":
+                case "phys ed":
+                case "physical education":
+                    Console.WriteLine("Thats a good subject for your physical health!");
+                    break;
+                case "history":
+                    Console.WriteLine("History is important so you don't repeat the bad things..");
+                    break;
+                case "english":
+                    Console.WriteLine("English is hard but also boring if its your primary language");
+                    break;
+                default:
+                    Console.WriteLine("I don't know that subject.");
+                    break;
+                     
+                    
+            }
+        }
+    }
+}
---
+++ Selection-Statements-Exercise/SelectionStatementExercise 1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Reflection.Metadata;
+
+namespace SelectionStatementExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Number Guessing Game
+            var r = new Random();
+            var favNumber = r.Next(1, 1001);
+            int userInput;
+            var counter = 0;
+
+            do
+            {
+
+                Console.WriteLine("Guess my favorite number between 1 - 1000!");
+                userInput = int.Parse(Console.ReadLine());
+
+                if (userInput < favNumber)
+                {
+                    Console.WriteLine($"{userInput} is too low");
+
+                }
+
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine($"{userInput} is too high");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it, are you a wizard!?");
+                }
+
+                counter++;
+
+                Console.WriteLine($"You have guessed {counter} times");
+
+            } while (userInput != favNumber);
+
+        }
+
+    }
+}
+
+
+
+
+
